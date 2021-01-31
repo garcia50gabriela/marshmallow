a55d35b407f6e9ba3b91a0366f988b4b0bd0bd90
garcia50gabriela <51689520+garcia50gabriela@users.noreply.github.com>
2021-01-31 13:56:29 -0600

    Add files via upload

---
+++ Assets/Scripts/achievement_display.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class achievement_display : MonoBehaviour
+{
+
+    public GameObject achievement_text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string text = "Badge \t\t\t Earned?\n\n";
+        foreach (KeyValuePair<int, game_data.Achievement> a in game_data.AchievementDict)
+        {
+            text += a.Value.Name + ":\t\t\t" + a.Value.Earned + "\n";
+        }
+        achievement_text.GetComponent<UnityEngine.UI.Text>().text = text;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/marshmallow_stick.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class marshmallow_stick : MonoBehaviour
+{
+    public float speed = 10f;
+    public Vector3 targetPos;
+    public Vector3 ogPos;
+    public bool isMoving;
+    public GameObject fire;
+    const int MOUSE = 0;
+    private float zpos;
+    // Use this for initialization1
+    void Start()
+    {
+
+        targetPos = transform.position;
+        ogPos = transform.position;
+        zpos = transform.position.z;
+        isMoving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move around fire
+        if (Input.GetMouseButton(MOUSE))
+        {
+            SetTargetPosition();
+        }
+        else
+        {
+            //SetOriginalPosition();
+        }
+        if (isMoving)
+        {
+            MoveObject();
+        }
+    }
+        
+    void SetTargetPosition()
+    {
+        Plane plane = new Plane(Vector3.forward, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float point = 0f;
+
+        if (plane.Raycast(ray, out point)) 
+        {
+            targetPos = ray.GetPoint(point);
+            if (targetPos.x < ogPos.x - 2.5 || targetPos.x > ogPos.x + 2.5)
+            {
+                targetPos = ogPos;
+            }
+            else 
+            {
+                targetPos.x = ogPos.x + ((ogPos.x - targetPos.x) * 5f);
+                targetPos.y = ogPos.y + ((ogPos.y - targetPos.y) * 5f);
+                targetPos.z = fire.transform.position.z;
+            }
+            
+        }
+
+        isMoving = true;
+    }
+    void SetOriginalPosition()
+    {
+        targetPos = new Vector3(ogPos.x, 100f, ogPos.y);
+
+        isMoving = true;
+    }
+    void MoveObject()
+    {
+        // pivot
+        transform.LookAt(targetPos);
+        
+        //3d ish
+        //transform.LookAt(fire.transform.position);
+
+        //transform.position = Vector3.MoveTowards(transform.position, new Vector3(targetPos.x, targetPos.y, zpos), speed * Time.deltaTime);
+        //if (transform.position == targetPos)
+        //isMoving = false;
+
+    }
+}
