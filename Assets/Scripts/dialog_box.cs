1d0ffb23031a38fc63fd96fad40294acb9f16e06
unknown <garcia50gabriela@gmail.com>
2021-03-06 00:28:34 -0600

    random dialog

--- Assets/Scripts/dialog_box.cs
+++ Assets/Scripts/dialog_box.cs
@@ -11,12 +11,42 @@ public class dialog_box : MonoBehaviour
     public GameObject followupText;
     public GameObject marshmallow;
     public GameObject[] raccoons;
+    public GameObject disablePanel;
+    public GameObject raccoonImage;
+    public GameObject possumImage;
     private float dialog_box_height;
     private Vector3 dialog_box_position;
     private float done_button_height;
     private Vector3 done_button_position;
     private float followup_box_height;
     private Vector3 followup_box_position;
+    private bool give_chosen = false;
+    private string[] raccoon_dialog = new string[] {
+        "Thanks pal, this looks almost as good as the trash I just ate.",
+        "Oh no, looks like I'll have to postpone my diet until tomorrow!",
+        "One man's trash is another raccoon's treasure.",
+        "Wow, I didn't even need to knock over a bin to get this.",
+        "For me? Thanks! And I'll I got you was rabies.",
+        "Ya know, I don't usually eat this sort of junk, but maybe just this once.",
+        "Oh it's my lucky day, it must be trash day!",
+        "Are you giving me this? Usually I just steal food!",
+        "Gross! I hope you washed your hands.",
+        "If you had thrown this out, I would've ended up eating it anyway.",
+        "This tastes like garbage! Compliments to the chef!",
+        "I wouldn't take this without saying thank you. That would be trashy."
+    };
+    private string[] eat_dialog = new string[] {
+        "marshmallow! yum!",
+        "now that's what I call R & R (roast and relaxation)!",
+        "toasty AND tasty",
+        "could this be more s'more scrumptious?!",
+        "that was a mouthwatering marshmallow!",
+        "...mmmmm... gooey..",
+        "flaming with flavor!",
+        "exquisite taste on a stick",
+        "like a heavenly cloud of sugar",
+        "delicious, divine, delectable, do I need say s'more?"
+    };
 
     // Start is called before the first frame update
     void Start()
@@ -46,6 +76,7 @@ public class dialog_box : MonoBehaviour
 
     public void show_dialog_box() 
     {
+        disablePanel.active = true;
         dialogBoxHolder.transform.position = new Vector3(dialog_box_position.x, dialog_box_position.y + dialog_box_height, dialog_box_position.z);
     }
 
@@ -61,8 +92,16 @@ public class dialog_box : MonoBehaviour
 
     public void hide_followup_box()
     {
+        if (give_chosen)
+        {
+            show_raccoon();
+        }
+        raccoonImage.active = false;
+        possumImage.active = false;
+        disablePanel.active = false;
         followupBoxHolder.transform.position = new Vector3(followup_box_position.x, followup_box_position.y - followup_box_height, followup_box_position.z);
         marshmallow.GetComponent<marshmallow>().re_enable_buttons();
+        give_chosen = false;
     }
 
     public void eat()
@@ -70,16 +109,17 @@ public class dialog_box : MonoBehaviour
         hide_marshmallow();
         hide_dialog_box();
         hide_done_button();
-        followupText.GetComponent<Text>().text = "Yum!";
+        followupText.GetComponent<Text>().text = eat_dialog[Random.Range(0, eat_dialog.Length)];
         show_followup_box();
     }
 
     public void give()
     {
+        give_chosen = true;
         hide_dialog_box();
         hide_done_button();
-        followupText.GetComponent<Text>().text = "You gave it to a nearby racoon, they seemed appreciative!";
-        show_raccoon();
+        followupText.GetComponent<Text>().text = raccoon_dialog[Random.Range(0, raccoon_dialog.Length)];
+        raccoonImage.active = true;
         hide_marshmallow();
         show_followup_box();
     }
@@ -97,8 +137,9 @@ public class dialog_box : MonoBehaviour
         }
         else 
         {
-            followupText.GetComponent<Text>().text = "Sorry, your marshmallow didn't pass inspection. Please try again!";
+            followupText.GetComponent<Text>().text = "Deepest apologies, your marshmallow didn't pass inspection. Please try again!";
         }
+        possumImage.active = true;
         show_followup_box();
     }
 
